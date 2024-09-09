db28bac0635efc138ecb71c258a88c5ab692a18c
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-09 21:27:04 +0300

    Game encoding

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+                //Console.WriteLine(key);
+                //ConsoleKey.
+                Console.WriteLine(key.ToString());
+            } while (key != ConsoleKey.Escape);
         }
     }
 }
