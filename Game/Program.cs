c88165593915460788a51adae2b30ba5230e0d05
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-11 19:41:14 +0300

    encoding

--- Game/Program.cs
+++ Game/Program.cs
@@ -11,33 +11,61 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите положение консоли по ординате X: ");
-            int cursorX = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите положение консоли по ординате Y: ");
-            int cursorY = Convert.ToInt32(Console.ReadLine());
-            while (true)
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.CursorVisible = false; // убрать предаток
+            Random rand = new Random();//генерация случ чисел, созд объект класса рандом
+            int cursorX = rand.Next(Console.BufferWidth); // возвар случ число типа инт
+            int cursorY = rand.Next(Console.BufferHeight); // возвар случ число типа инт
+            Console.WriteLine($"X = {cursorX}, Y = {cursorY}");
+            Console.SetCursorPosition(cursorX, cursorY);
+            char symbol = (char)2;
+            Console.WriteLine(symbol);
+            ConsoleKey key;
+            do
             {
-                Console.Clear();                                //отчищаем консоль
-                Console.SetCursorPosition(cursorX, cursorY);      //координты курсора
-                Console.Write(Convert.ToChar("☺"));            // прорисовка курсора/рожицы
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                switch (keyInfo.Key)
+                switch (key)
                 {
-                    case ConsoleKey.W:
-                        cursorY--;
-                        break;
-                    case ConsoleKey.S:
-                        cursorY++;
-                        break;
-                    case ConsoleKey.A:
-                        cursorX--;
-                        break;
-                    case ConsoleKey.D:
-                        cursorX++;
-                        break;
-                    case ConsoleKey.Escape:
-                        return;
+                    case ConsoleKey.W: cursorY--; break;
+                    case ConsoleKey.S: cursorY++; break;
+                    case ConsoleKey.A: cursorX -= 2; break;
+                    case ConsoleKey.D: cursorX += 2; break;
                 }
+                if (cursorY < 0) cursorY = 0;
+                if (cursorX < 0) cursorX = 0;
+                if (cursorY > Console.BufferHeight - 1) cursorY = Console.BufferHeight - 1;
+                if (cursorX > Console.BufferWidth - 1) cursorX = Console.BufferWidth - 1;
+                Console.Clear();
+                Console.SetCursorPosition(cursorX, cursorY);
+
+            }
+            //Console.Write("Введите положение консоли по ординате X: ");
+            //int cursorX = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите положение консоли по ординате Y: ");
+            //int cursorY = Convert.ToInt32(Console.ReadLine());
+            //while (true)
+            //{
+            //    Console.Clear();                                //отчищаем консоль
+            //    Console.SetCursorPosition(cursorX, cursorY);      //координты курсора
+            //    Console.Write(Convert.ToChar("☺"));            // прорисовка курсора/рожицы
+            //    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            //    switch (keyInfo.Key)
+            //    {
+            //        case ConsoleKey.W:
+            //            cursorY--;
+            //            break;
+            //        case ConsoleKey.S:
+            //            cursorY++;
+            //            break;
+            //        case ConsoleKey.A:
+            //            cursorX--;
+            //            break;
+            //        case ConsoleKey.D:
+            //            cursorX++;
+            //            break;
+            //        case ConsoleKey.Escape:
+            //            return;
+            //    }
                 #region MyRegion
                 //ConsoleKey key;
                 //do
