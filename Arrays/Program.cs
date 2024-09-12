08bcd85f9316e8f8ba795020183ea610e9b9cdcd
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-12 19:54:32 +0300

    ARRAYS_1_and_ARRAYS_2 encoding

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,10 +1,11 @@
 ﻿//#define ARRAYS_1
-//#define ARRAYS_2
-#define JAGGED_ARRAYS
+#define ARRAYS_2
+//#define JAGGED_ARRAYS
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 //1) Single dimensional;
@@ -24,7 +25,7 @@ namespace Arrays
             int n = Convert.ToInt32(Console.ReadLine());
             int[] arr = new int[n];
             //Заполняем массив случ числами
-            Random rand = new Random(0);
+            Random rand = new Random();
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = rand.Next(50, 100);
@@ -34,7 +35,36 @@ namespace Arrays
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write(arr[i] + "\t");
-            } 
+            }
+
+            //Среднее-арифметическое
+            double sred = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sred += arr[i];
+            }
+            sred /= arr.Length;
+            Console.WriteLine();
+            Console.WriteLine("Среднее-арифметическое равно: " + sred);
+
+            //Минимальное и максимальное
+            double max = arr[0];
+            double min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Минимальое равно: " + min);
+            Console.WriteLine();
+            Console.WriteLine("Максимальое равно: " + max);
 #endif
 
 #if ARRAYS_2
@@ -51,7 +81,7 @@ namespace Arrays
             //Console.WriteLine(new int[2,3,4,5].Rank);
             Console.WriteLine($"Количество измерений массива: {i_arr_2.Rank}");
             Console.WriteLine($"Количество элементов в нулевом изм (кол строк): {i_arr_2.GetLength(0)}");
-            Console.WriteLine($"Количество элементов в нулевом изм (кол строк): {i_arr_2.GetLength(1)}");
+            Console.WriteLine($"Количество элементов в нулевом изм (кол столбов): {i_arr_2.GetLength(1)}");
             for (int i = 0; i < i_arr_2.GetLength(0); i++)
             {
                 for (int j = 0; j < i_arr_2.GetLength(1); j++)
@@ -62,11 +92,46 @@ namespace Arrays
             }
             Console.WriteLine();
 
-            foreach (int i in i_arr_2)
+            /*foreach (int i in i_arr_2)
             {
                 Console.Write(i + "\t");
             }
-            Console.WriteLine(); 
+            Console.WriteLine();*/
+
+                 //Среднее-арифметическое
+            double sred = 0;
+            int count = 0;   // колич.элементов массива
+            for (int i = 0; i < i_arr_2.GetLength(0); i++) 
+            {
+                for (int j = 0; j < i_arr_2.GetLength(1); j++)
+                {
+                    sred = i_arr_2[i, j];
+                    count++;
+                }
+            }
+            sred /= count;
+            Console.WriteLine();
+            Console.WriteLine("Среднее-арифметическое равно: " + sred);
+
+            // Минимальное и Максимальное
+            double max = i_arr_2[0, 0];
+            double min = i_arr_2[0, 0];
+            for (int i = 0; i < i_arr_2.GetLength(0); i++)
+            {
+                for (int j = 0; j < i_arr_2.GetLength(1); j++)
+                {
+                    if (i_arr_2[i, j] < min)
+                    {
+                        min = i_arr_2[i, j];
+                    }
+                    if (i_arr_2[i, j] > max)
+                    {
+                        max = i_arr_2[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Минимальное равно: " + min);
+            Console.WriteLine("Максимальное равно: " + max);
 #endif
 
 #if JAGGED_ARRAYS
