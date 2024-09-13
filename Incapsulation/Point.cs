88750d4837a5dc843bda711d86f81bbbded90221
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-13 19:59:39 +0300

    encoding

---
+++ Incapsulation/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation
+{
+    internal class Point
+    {
+        double x, y;
+        public double GetX()
+        {
+            return x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"X={x}, Y={y}");
+        }
+    }
+}
---
+++ Incapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point A = new Point(); //оператор 'new' вызывает конструктор класса.
+            A.SetX(2);
+            A.SetY(3);
+            Console.WriteLine($"X={A.GetX()}, Y={A.GetY()}");
+            A.Print();
+        }
+    }
+}
