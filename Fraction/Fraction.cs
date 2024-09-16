551caa8324146c627d3ceefcad46b836c21f7737
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-16 20:40:24 +0300

    encoding

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,50 @@
+﻿#define HHHHHHH
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        public int Integer { get; set; } //автосвойства, котог=рые не требуют объявления 
+        //переменной поскольку сами не явно её объявляют
+        public int Numerator { get; set; }
+        ///  /////////////////////
+        int denominator;
+        // Обычные свойства которые применяются к переменной  'denominator':
+        public int Denominator
+        {
+            get => denominator;
+            set => denominator = value == 0 ? 1 : value;
+        }
+        ///  /////////////////////
+
+        //                Constructors:
+        Fraction(int integer)
+        {
+            this.Integer = integer;
+            Console.WriteLine($"1ArgConstructor:{GetHashCode()}");
+        }
+        ~Fraction()
+        {
+            Console.WriteLine($"Destructor:{GetHashCode()}");
+        }
+
+        //                 Methods:
+        public void Print()
+        {
+            if (Integer != 0) Console.Write(Integer);
+            if (Numerator != 0)
+            {
+                Console.Write("(");
+                Console.Write($"{Numerator}/{Denominator}");
+                Console.Write(")");
+            }
+            else if (Integer == 0) Console.Write(0);
+            Console.WriteLine();
+        }
+    }
+}
