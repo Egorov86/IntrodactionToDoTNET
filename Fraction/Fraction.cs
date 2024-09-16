45decc5997cd855d4e631595259fb6c4f7d2bde4
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-16 21:39:10 +0300

    encoding

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -1,7 +1,7 @@
-﻿#define HHHHHHH
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -38,12 +38,53 @@ namespace Fraction
             this.Denominator = denominator;
             Console.WriteLine($"Constructor:{GetHashCode()}");
         }
+        public Fraction(int integer, int numerator, int denominator)
+        {
+            this.Integer = integer;
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:{GetHashCode()}");
+        }
+        public Fraction(Fraction other)
+        {
+            this.Integer = other.Integer;
+            this.Numerator = other.Numerator;
+            this.Denominator = other.Denominator;
+            Console.WriteLine($"CopyConstructor:{GetHashCode()}");
+        }
         ~Fraction()
         {
             Console.WriteLine($"Destructor:\t{GetHashCode()}");
         }
+        //                  OPERATOR
+        public static Fraction operator *(Fraction l, Fraction r)
+        {
+            Fraction left = l.Improper();
+            Fraction right = r.Improper();
+            Fraction res = new Fraction
+                (
+                left.Numerator * right.Numerator,
+                left.Denominator * right.Denominator
+                );
+            return res;
+
+        }
 
         //                 Methods:
+        Fraction Proper()
+        {
+            Fraction copy = new Fraction(this);
+            copy.Integer += copy.Numerator / copy.Denominator;
+            copy.Numerator %= copy.Denominator;
+            return copy;
+        }
+        Fraction Improper()
+        {
+            Fraction copy = new Fraction(this);
+            copy.Numerator += copy.Integer * copy.Denominator;
+            copy.Integer = 0;
+            return copy;
+        }
         public void Print()
         {
             if (Integer != 0) Console.Write(Integer);
@@ -55,6 +96,21 @@ namespace Fraction
             }
             else if (Integer == 0) Console.Write(0);
             Console.WriteLine();
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            string print = "";
+            if (Integer != 0) print += Integer;
+            if (Numerator != 0)
+            {
+                if (Integer !=0) print += "(";
+                print += $"{Numerator}/{Denominator}";
+                if (Integer != 0) print += ")";
+            }
+            else if (Integer == 0) print += (0);
+            return print;
+
         }
     }
 }
