2b36dff80ffb19e4711f583a70f48a1a96ae3d4a
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-17 17:51:40 +0300

    Point encoding

--- Incapsulation/Point.cs
+++ Incapsulation/Point.cs
@@ -6,52 +6,50 @@ using System.Threading.Tasks;
 
 namespace Incapsulation
 {
-    internal class Point
+    class Point
     {
-        /*double x, y;
-        public double X
-        {
-            get
-            {
-                return x;
-            }
-            set
-            {
-                //if(value...)...
-                x = value; // value - открыть доступ
-            }
-        }
-        public double Y
-        {
-            get
-            {
-                return y;
-            }
-            set
-            {
-                y = value;
-            }
-        }
-        public double GetX()
-        {
-            return x;
-        }
-        public double GetY()
-        {
-            return y;
-        }
-        public void SetX(double x)
-        {
-            this.x = x;
-        }
-        public void SetY(double y)
-        {
-            this.y = y;
-        }*/
-        public double X { get; set; }
-        public double Y { get; set; }
+        /*double x;
+		double y;
+		public double X
+		{
+			get
+			{
+				return x;
+			}
+			set
+			{
+				//if(value...)......
+				x = value;	//value - открывает доступ к переданному значению
+			}
+		}
+		public double Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
 
-        public Point(double x = 0, double y = 10)
+		public double GetX()
+		{
+			return x;
+		}
+		public double GetY()
+		{
+			return y;
+		}
+		public void SetX(double x)
+		{
+			this.x = x;	//В языке C# отсутствуют указатели, а следовательно отсутсвует оператор '->'
+		}
+		public void SetY(double y)
+		{
+			this.y = y;
+		}*/
+
+        public double X { get; set; }   //Автосвойства для 'X'
+        public double Y { get; set; }   //Автоствойства для 'Y'
+
+        //			  Constructors:
+        public Point(double x = 0, double y = 0)
         {
             X = x;
             Y = y;
@@ -61,15 +59,15 @@ namespace Incapsulation
         {
             this.X = other.X;
             this.Y = other.Y;
-            Console.WriteLine($"CopyConstructor:\t{this.GetHashCode()}");
+            Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
         }
         ~Point()
         {
-            Console.WriteLine($"Destructor\t{this.GetHashCode()}");
+            Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
         }
 
-        //   Opertor:
-        public static Point operator+(Point left, Point right)
+        //				Operators:
+        public static Point operator +(Point left, Point right)
         {
             Point res = new Point();
             res.X = left.X + right.X;
@@ -92,7 +90,10 @@ namespace Incapsulation
         }
 
         //   Methods:
-        public double Distance(Point other) // метод Distance
+
+        // ДОМАШНЯЯ РАБОТА
+
+        /*public double Distance(Point other) // метод Distance
         {
             double dx = this.X - other.X;
             double dy = this.Y - other.Y;
@@ -103,7 +104,7 @@ namespace Incapsulation
             double dx = a.X - b.X;
             double dy = a.Y - b.Y;
             return Math.Round(Math.Sqrt(dx * dx + dy * dy), 3);
-        }
+        }*/
         public void Print()
         {
             Console.WriteLine($"X={X}, Y={Y}");
