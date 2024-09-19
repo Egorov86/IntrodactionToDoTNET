ba2c76e52e433c36a34ef7b6b277e60f82dc7991
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-19 18:53:02 +0300

    encoding

---
+++ Academy_1/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Academy_1
+{
+    public class Student : Human
+    {
+        public string Speciality { get; set; }
+        public string Group { get; set; }
+        public double Rating { get; set; }
+        public double Attendance { get; set; }
+    }
+    public Student(string lastName, string firstName, uint age,
+    string speciality, string group, double rating, double attendance
+    ) : base(lastName, firstName, age)
+    {
+        Speciality = speciality;
+        Group = group;
+        Rating = rating;
+        Attendance = attendance;
+        Console.WriteLine($"SConstructor:\t{GetHashCode()}");
+    }
+    public Student(Human human, string speciality, string group, double rating, double attendance) : base(human)
+    {
+
+    }
+    ~Student()
+    {
+        Console.WriteLine($"Destructor:\t{GetHashCode()}");
+    }
+    public override string Tostring()
+    {
+        return base.ToString() + $"{S}";
+    }
+}
---
+++ Academy_1/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using Academy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy_1
+{
+    internal class Teacher
+    {
+        public class Teacher : Human
+        {
+            public string Speciality { get; set; }
+            public string Group { get; set; }
+            public double Rating { get; set; }
+            public uint Experience { get; set; }
+            public Teacher(Human human, string speciality)
+        }
+    }
+}
