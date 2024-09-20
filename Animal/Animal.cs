6c280f861d47d2198b8042a8089ebc0e8992f3cf
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-09-20 21:31:39 +0300

    en

---
+++ Animal/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    abstract class Animal
+    {
+       //Animal public abstract void = new Animal(); //Создать экзм абстракт класса не возможно
+
+    }
+}
---
+++ Animal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal public abstract void = new Animal(); 
+            //Создать экзм абстракт класса не возможно
+            Wolf wolf = new Wolf();
+            wolf.Sound();
+
+            Chihuahua nedosobaka = new Chihuahua();
+            nedosobaka.Sound();
+        }
+    }
+}
